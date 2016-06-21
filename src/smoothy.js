8494273f141fa739eb5eba70c9643b7997a665c7
Mattias Haal <mattias.haal@grandpublic.se>
2016-06-21 19:11:03 +0200

    Clean up spaces.

--- src/smoothy.js
+++ src/smoothy.js
@@ -9,59 +9,56 @@
  */
 
 (function(root) {
-	'use strict';
-	var fixedHeaderHeight = 50, // If UI using fixed header. Write the height of your header to avoid your target being hidden behind it.
-		movingFrequency = 15, // Affect performance.
-		links = document.getElementsByTagName('a'),
-		href;
+    'use strict';
+    var fixedHeaderHeight = 50, // If UI using fixed header. Write the height of your header to avoid your target being hidden behind it.
+        movingFrequency = 15, // Affect performance.
+        links = document.getElementsByTagName('a'),
+        href;
 
-	for (var i = 0; i < links.length; i++) {
-		href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
-		if (href !== null && href.length > 1 && href.indexOf('#') != -1) {
-			links[i].onclick = function() {
-				var element,
-					href = this.attributes.href.nodeValue.toString(),
-	                url = href.substr(0, href.indexOf('#')),
-	                id = href.substr(href.indexOf('#') + 1),
-	                speed = 2; // Set 'speed' value in px/ms. Higher number makes the scroll faster. Important notice: Make sure the generated 'time' value is greater than 'movingFrequency'.
+    for (var i = 0; i < links.length; i++) {
+        href = (links[i].attributes.href === undefined) ? null : links[i].attributes.href.nodeValue.toString();
+        if (href !== null && href.length > 1 && href.indexOf('#') != -1) {
+            links[i].onclick = function() {
+                var element,
+                    href = this.attributes.href.nodeValue.toString(),
+                    url = href.substr(0, href.indexOf('#')),
+                    id = href.substr(href.indexOf('#') + 1),
+                    speed = 2; // Set 'speed' value in px/ms. Higher number makes the scroll faster. Important notice: Make sure the generated 'time' value is greater than 'movingFrequency'.
 
-	            if (element = document.getElementById(id)) {
-	            	var getScrollTopDocumentAtBegin = getScrollTopDocument(),
-	            		distance = getScrollTopElement(element) - getScrollTopDocumentAtBegin,
-	            		time = Math.abs(distance / speed),
-						scrollCount = (time - (movingFrequency % time)) / movingFrequency, // Always make an integer.
-	            		distance = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / scrollCount;
+                if (element = document.getElementById(id)) {
+                    var getScrollTopDocumentAtBegin = getScrollTopDocument(),
+                        distance = getScrollTopElement(element) - getScrollTopDocumentAtBegin,
+                        time = Math.abs(distance / speed),
+                        scrollCount = (time - (movingFrequency % time)) / movingFrequency, // Always make an integer.
+                        distance = (getScrollTopElement(element) - getScrollTopDocumentAtBegin) / scrollCount;
 
-	            	if (window.history && typeof window.history.pushState == 'function') {
-	            		window.history.pushState({}, undefined, url + '#' + id); // Change URL for modern browser.
-	            	}
+                    if (window.history && typeof window.history.pushState == 'function') {
+                        window.history.pushState({}, undefined, url + '#' + id); // Change URL for modern browser.
+                    }
 
-	            	for (var i = 1; i <= scrollCount; i++) {
-	            		(function() {
+                    for (var i = 1; i <= scrollCount; i++) {
+                        (function() {
                             var scrollPosition = distance * i;
                             setTimeout(function() {
                                 window.scrollTo(0, scrollPosition + getScrollTopDocumentAtBegin); 
                             }, movingFrequency * i);
                         })();
-	            	}
-	            	return false;
-	            }
-			};
-		} 
-	}
+                    }
+                    return false;
+                }
+            };
+        } 
+    }
 
-	var getScrollTopElement = function(e) {
+    var getScrollTopElement = function(e) {
         var top = fixedHeaderHeight * -1;
-
-        while (e.offsetParent != undefined && e.offsetParent != null)
-        {
+        while (e.offsetParent != undefined && e.offsetParent != null) {
             top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
             e = e.offsetParent;
         }
-
         return top;
     };
-    
+        
     var getScrollTopDocument = function() {
         return window.pageYOffset !== undefined ? window.pageYOffset : document.documentElement.scrollTop !== undefined ? document.documentElement.scrollTop : document.body.scrollTop;
     };
