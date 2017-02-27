480faf7981538be7874b826888ffe8e26af0cee6
Mattias Haal <mattias.haal@grandpublic.se>
2017-02-27 09:12:44 +0100

    Add babel.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,40 @@
-/*  Gulp setup
-============================================================================ */
+// --------------------------------------------------------
+// Gulp setup
+// --------------------------------------------------------
 
 /* Load plugins */
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var header = require('gulp-header');
-var plumber = require('gulp-plumber');
-var uglify = require("gulp-uglify");
-var package = require('./package.json');
+var gulp    = require('gulp');
+var babel   = require('gulp-babel');
+var uglify  = require('gulp-uglify');
+var concat  = require('gulp-concat');
+var header  = require('gulp-header');
+var pkg     = require('./package.json');
 
 /* Banner */
 var banner = [
-    '/* ',
-        '<%= package.name %> ',
-        'v<%= package.version %> | ',
-        '© ' + new Date().getFullYear() + ' <%= package.author %> | ',
-        '<%= package.repository.url %>',
-    ' */',
-    '\n'
+
+	'/*! ',
+		'<%= pkg.title %> | ',
+		'v<%= pkg.version %> | ',
+		'<%= pkg.author %>',
+	' */',
+	'\n'
+
 ].join('');
 
-gulp.task("scripts", function() {
-    gulp.src([
-        "src/smoothy.js"
+/* Scripts */
+gulp.task('app-js', function() {
+
+  return gulp.src([
+      './src/smoothy.js'
     ])
-    .pipe(plumber())
-    .on("error", function(err) {
-        console.log(err.message);
-    })
-    .pipe(concat("smoothy.min.js"))
+    .pipe(babel())
+    .pipe(concat('smoothy.min.js'))
     .pipe(uglify())
-    .pipe(header(banner, {
-        package: package
-    }))
-    .pipe(gulp.dest("dist"))
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('./dist'))
+
 });
 
 /* Run Gulp */
-gulp.task('default', ['scripts']);
+gulp.task('default', ['app-js']);
